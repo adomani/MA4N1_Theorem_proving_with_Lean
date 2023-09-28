9802d015c4289ab7acf256e2d6dd59f17a0cf715
adomani <adomani@gmail.com>
2023-09-28 17:45:50 +0200

    First files

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «mA4N1_2023» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «MA4N12023» {
+  -- add any library configuration options here
+}
