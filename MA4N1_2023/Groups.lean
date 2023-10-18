1cf10e340ac5c9ca6b2a63388255bb8a5fa8819e
adomani <adomani@gmail.com>
2023-10-18 23:38:21 +0100

    Add more exercises

--- MA4N1_2023/Groups.lean
+++ MA4N1_2023/Groups.lean
@@ -1,5 +1,7 @@
 import Mathlib.Tactic
 
+namespace TPwL
+
 /-!
 #  Groups
 
@@ -70,11 +72,157 @@ Two comments.
 #  Exercises
 -/
 
-example {a : G} : (a⁻¹)⁻¹ = a := by
-  sorry
+variable (a b c : G)
 
-example {a b c : G} : a * (b * c) * a⁻¹ = (a * b * a⁻¹) * (a * c * a⁻¹) := by
-  sorry
+example : (a⁻¹)⁻¹ = a := by
+  exact inv_inv a
+  done
 
-example {a b c : G} : a⁻¹ * (b * c) * a = (a⁻¹ * b * a) * (a⁻¹ * c * a) := by
+lemma conj_mul : a * (b * c) * a⁻¹ = (a * b * a⁻¹) * (a * c * a⁻¹) := by
+  exact conj_mul.symm
+  done
+
+example : a⁻¹ * (b * c) * a = (a⁻¹ * b * a) * (a⁻¹ * c * a) := by
+  -- the "boring" way
+  simp [← mul_assoc]
+  done
+
+example : a⁻¹ * (b * c) * a = (a⁻¹ * b * a) * (a⁻¹ * c * a) := by
+  -- the "recycle the lemma we already proved" way
+  convert conj_mul a⁻¹ b c <;>
+  simp
+  done
+
+/-
+For elements of groups, we may be interested in computing their order.
+Mathlib calls the order `orderOf g` (and `addOrderOf a` in the case of an additive group).
+-/
+
+--  In fact, the order is defined for a `Monoid`, not only for groups.  Oh, well.
+#check orderOf
+
+-- feel free to free-style this, but read on if you want hints!
+example : addOrderOf (2 : ZMod 5) = 5 := by
   sorry
+  done
+
+/-
+In the example above, you may be in the following situation:
+* you (hopefully) know the meaining of `orderOf/addOrderOf` in maths;
+* you are confronted with the Mathlib implementation of that definition.
+
+How do we proceed?
+
+First, we could "Go to definition" and check if `Mathlib`s definition happens to be
+directly mirroring the one that we already know:
+-/
+
+#print orderOf
+/-
+def orderOf.{u_1} : {G : Type u_1} → [inst : Monoid G] → G → ℕ :=
+fun {G} [Monoid G] x => Function.minimalPeriod (fun x_1 => x * x_1) 1
+
+Ok, that probably did not go according to plan.
+Even cleaning the output above for readability:
+
+def orderOf : the inputs are a `Monoid` `G` and an element of `G`; the output is in `ℕ`
+The processed definition is:
+if `x ∈ G`, then `orderOf x` is the "`Function.minimalPeriod`" of the function
+`G → G` given by `y => x * y`.
+
+We can probably guess what `minimaPeriod` is,
+we can probably convince ourselves that this coincides with our "standard" definition.
+
+However, whoever wrote this definition did it because it "worked better for Lean",
+but surely they must have wanted the definition that we expect!
+
+This means that we should look for a lemma the proves that whatever the actual
+`Mathlib` implementation is, it is equivalent to the "mathematical" one.
+
+Such lemmas are often named `<property>_def`, `<property>_iff`, or something like this.
+-/
+
+#check orderOf_eq_iff
+/-
+orderOf_eq_iff.{u_1} {G : Type u_1} [inst✝ : Monoid G] {x : G} {n : ℕ} (h : 0 < n) :
+  orderOf x = n ↔ x ^ n = 1 ∧ ∀ (m : ℕ), m < n → 0 < m → x ^ m ≠ 1
+
+**Much better!**
+Longer, but better!
+Hopefully, you recognise the assertion now:
+assuming the `n` is strictly positive,
+the statement `orderOf x = n` is equivalent to the conditions
+* `x ^ n = 1`
+* for all `m` strictly between `0` and `n`, `x ^ m` is not `1`.
+
+I hope that you recognise this!
+
+Thus, presumably when you are getting familiar with `orderOf`, you will begin by
+using `rw [orderOf_eq_iff]` and continue working with the definition that you know.
+(Note that `rw` works not only with equalities, but also with iffs.)
+
+*Aside.*
+Probably, in the long run, you may appreciate the *actual* implementation and you will
+be able to take advantage also of that, but, thanks for the equivalence,
+in theory there is no need.
+
+One more twist: what replaces the power in `x ^ n = 1` in the case of an `AddGroup`?
+Let's find out!
+-/
+
+#check addOrderOf_eq_iff
+/-
+addOrderOf_eq_iff.{u_1} {G : Type u_1} [inst✝ : AddMonoid G] {x : G} {n : ℕ} (h : 0 < n) :
+  addOrderOf x = n ↔ n • x = 0 ∧ ∀ (m : ℕ), m < n → 0 < m → m • x ≠ 0
+
+So, `x ^ n = 1` became `n • x = 0`.
+That may look more or less reasonable, except, possibly for `•`:
+this is the "scalar product" operation.
+In this case, it allows you to multiply a natural number `n` and an element of a generic
+`AddGroup` `x`.
+Indeed, it is just like the power, but for iterated addition.
+In easy cases, using `simp` may clear this.
+If you find yourself having to actually work with `•`, note that its name is
+`nsmul` (`s`calar `mul`tiplication by `n`atural numbers -- there is also `smul`).
+Thus, you may look for lemmas involving `nsmul` to help out -- but this will not
+be necessary in this exercise sheet!
+
+Let's go back to proving the result that we stated above.
+-/
+
+example : addOrderOf (2 : ZMod 5) = 5 := by
+  rw [addOrderOf_eq_iff]
+  simp
+  norm_num
+  done
+
+example : addOrderOf (3 : ZMod 6) = 2 := by
+  rw [addOrderOf_eq_iff]
+  simp
+  norm_num
+  done
+
+/-
+Finally, a small quirk:
+`orderOf/addOrderOf` returns a natural number for *every* element of every (`Add`)`Group`.
+Also for the elements that may not have finite order.
+What is the order of an element that does not have finite order?
+Well, it must be a natural number and `Mathlib`s convention is that it is `0`.
+
+In this case, a relevant lemma is `addOrderOf_eq_zero_iff'`.
+
+Here is an example.
+-/
+
+example : addOrderOf (1 : ℤ) = 0 := by
+  rw [addOrderOf_eq_zero_iff']
+  intros n n0
+  simp
+  exact Nat.pos_iff_ne_zero.mp n0
+  done
+
+/-
+
+-/
+
+end TPwL
