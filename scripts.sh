252dda5886fa74596c9d2486130cfa6a515d1d94
adomani <adomani@gmail.com>
2023-10-31 19:46:09 +0000

    Add mkNew

--- scripts.sh
+++ scripts.sh
@@ -13,3 +13,15 @@ exme () {
   echo ${newPth}
   exercify "${1}" > "${newPth}"
 }
+
+mkNew () {
+  local tl="$(ls MA4N1_2023/L* | sed -n 's=MA4N1_2023/L0*\([0-9][0-9]*\)_.*=\1=p' | tail -1)"
+  tl="$(printf '%02d' "$(( tl + 1 ))")"
+  local file="$( printf 'MA4N1_2023/L%s_%s.lean' "${tl}" "${1// /_}" )"
+  if [ -f "${file}" ]; then
+    lcyan $'File' ; printf ' %s ' "${file}" ; lcyan $'already exists!\n'
+  else
+    { brown $'save to file '; printf '%s\n' "${file}" ; } >&2
+    printf $'import Mathlib.Tactic\n\nnamespace TWwL\n\n\n\nend TPwL\n' > "${file}"
+  fi
+}
