8cc905eaf9970b0721ad9891efc7abfab8895036
adomani <adomani@gmail.com>
2023-11-01 23:38:41 +0000

    rcases and more

--- MA4N1_2023/L08_div_induction.lean
+++ MA4N1_2023/L08_div_induction.lean
@@ -3,6 +3,69 @@ import Mathlib.Tactic
 namespace TPwL
 
 /-!
+
+#  Divisibility as an excuse to see more tactics
+
+This exercise sheet proves some results on divisibility among natural numbers.
+
+Before going into the actual exercises, I want to introduce the tactic `rcases`.
+It is personally one of my favourite tactics, although it took me a while to
+appreciate how useful it can be!
+You would be surprised by how far you can get by with just `refine/apply` and `rcases`!
+
+##  Digression on `rcases`
+
+The `rcases` tactic is the recursive version of `cases`.
+It has lot's of features, but in its most basic form it allows us to avoid long chains
+of `cases x with ...`.
+Here is a brief summary of some of its features.
+
+If we have an assumption `h : p ∧ q` in our local context, then `rcases h with ⟨hp, hq⟩`
+"deconstructs" the `And` and leaves the two hypotheses `hp : p, hq : q` in its place.
+
+If we have an assumption `h : p ∨ q` in our local context, then `rcases h with hp | hq`
+"deconstructs" the `Or` and produces two goals, one containing the hypothesis `hp : p`,
+the other containing `hq : q`.
+
+These deconstructions can be combined recursively.
+-/
+
+example {m n : ℕ} (h : (m = 0 ∧ n = 1) ∨ (m = 0 ∧ n = 2)) : m = 0 := by
+  rcases h with ⟨m0, _n1⟩ | ⟨m0, _n2⟩ <;>
+  exact m0
+  done
+
+/-!
+... and it can be used on `inductive` types as well:
+-/
+example {n : ℕ} : (n = 0) ∨ (n = 1) ∨ (n = 2) ∨ (3 ≤ n) := by
+  rcases n with _ | _ | _ | _ <;>
+  simp
+  exact? says exact tsub_add_cancel_iff_le.mp rfl
+  done
+
+/-!
+One more feature: if destructuring you encounter an equality where one of the two
+sides is a variable and the other side does not contain the variable,
+you can write `rfl` in the corresponding `rcases` slot and `rcases` replaces
+all occurrences of that variable by its value.
+-/
+
+example {n : ℕ} (h : (n = 0) ∨ (n = 1) ∨ (n = 2) ∨ (n = 3) ∨ (n = 4)) : n ≤ 4 := by
+  rcases h with rfl | rfl | rfl | rfl | rfl <;>
+    repeat apply Nat.succ_le_succ
+  all_goals exact Nat.zero_le _
+  done
+
+/-!
+Take a look at the documentation for `rcases` to see more features.
+-/
+
+#help tactic rcases
+
+/-!
+Now that the aside is over, let's go to divisibility.
+
 First, we begin with a warm-up working with divisors.
 The symbol for "divides" is `∣`, that is `\mid`.
 The symbol for "such that" that appears in the definition of a set is `|` -- they look the same, but are not the same!
@@ -15,7 +78,7 @@ Note that if `a : ℕ`, then the lower bound of `0` is always available,
 so you only need to have in context an upper bound of the form `a ≤ [some number]`.
 However, if there is a better lower bound than `0`, then `interval_cases a` will use it.
 
-You can take a look at the documentation for `interval_cases here:
+You can take a look at the documentation for `interval_cases` here:
 -/
 #help tactic interval_cases
 
@@ -41,7 +104,7 @@ example : {a : ℕ | a ∣ 6} = {1, 2, 3, 6} := by
       · exact h
     interval_cases a <;> simp_all
     done
-  · cases h with
+  · cases h with   -- this chain is a prime candidate for `rcases`!
       | inl h =>
         simp_all
       | inr h =>
@@ -119,5 +182,91 @@ around the `Decidable` instances and working with `Finset`s.
 
 Note that working with `Finset`s can be annoying!
 -/
+
+lemma dvd_induction {P : ℕ → Prop} (n : ℕ)
+    (P0 : P 0)
+    (P1 : P 1)
+    (P_mul : ∀ {p a}, Nat.Prime p → a ≠ 0 → a ≠ 1 → P a → P (p * a))
+    (P_prime : ∀ {p}, Nat.Prime p → P p) :
+    P n := by
+  apply Nat.strongInductionOn
+  intros n hn
+  by_cases h : n ≤ 1
+  · interval_cases n <;> assumption
+  have := Nat.exists_prime_and_dvd (ne_of_not_le h)
+  rcases this with ⟨p, pPrime, ⟨q, rfl⟩⟩
+  rcases q with _ | _ | q
+  · simpa
+  · simp [P_prime pPrime]
+  · apply P_mul pPrime (Nat.succ_ne_zero _) q.succ_succ_ne_one
+    · apply hn
+      apply (one_mul _).symm.le.trans_lt
+      apply Nat.mul_lt_mul_of_pos_right pPrime.one_lt
+      exact Nat.succ_pos _
+
+open Pointwise
+
+lemma _root_.Nat.Prime.divisors_mul (n : ℕ) {p : ℕ} (hp : Nat.Prime p) :
+    Nat.divisors (p * n) = Nat.divisors p * Nat.divisors n := by
+  ext a
+  rw [Finset.mem_mul]
+  simp? [hp.divisors, dvd_mul, Nat.dvd_prime hp] says
+    simp only [Nat.mem_divisors, Nat.isUnit_iff, dvd_mul, Nat.dvd_prime hp, exists_and_left,
+      exists_eq_or_imp, one_mul, exists_eq_right', exists_eq_left, ne_eq, mul_eq_zero, hp.divisors,
+      Finset.mem_singleton, Finset.mem_insert, exists_eq_right]
+  aesop
+  done
+
+example {m n : ℕ} : Nat.divisors m * Nat.divisors n = Nat.divisors (m * n) := by
+  apply dvd_induction m
+  · simp only [Nat.divisors_zero, Finset.empty_mul, zero_mul, forall_const]
+  · simpa using one_mul _
+  · intros p a hp _ _ han
+    rw [hp.divisors_mul, mul_assoc p, hp.divisors_mul, mul_assoc, han]
+  · intros p hp
+    exact (hp.divisors_mul _).symm
+
+
+#exit
+
+lemma dvd_induction {P : ℕ → Prop} (n : ℕ)
+    (P0 : P 0)
+    (P1 : P 1)
+    (P_mul : ∀ {a b}, a ≠ 0 → a ≠ 1 → b ≠ 0 → b ≠ 1 → P a → P b → P (a * b))
+    (P_prime : ∀ {p}, Nat.Prime p → P p) :
+    P n := by
+  apply Nat.strongInductionOn
+  intros n hn
+  by_cases h : n ≤ 1
+  · interval_cases n <;> assumption
+  have := Nat.exists_prime_and_dvd (ne_of_not_le h)
+  rcases this with ⟨p, pPrime, ⟨q, rfl⟩⟩
+  cases q with
+  | zero => simpa
+  | succ q =>
+    cases q with
+    | zero =>
+      simp [P_prime pPrime]
+    | succ q =>
+      apply P_mul pPrime.ne_zero pPrime.ne_one ?_ ?_ (P_prime pPrime)
+      · apply hn
+        convert Nat.mul_lt_mul_of_pos_right pPrime.one_lt ?_ using 1
+        simp
+        exact Nat.succ_pos _
+      · exact Nat.succ_ne_zero _
+      · exact Nat.succ_succ_ne_one q
+  done
+
+open Lean
+
+#eval show IO _ from do
+  let bd := 10
+  for m in [:bd] do
+    for n in [:bd] do
+      if decide <| Nat.divisors m * Nat.divisors n = Nat.divisors (m * n) then
+        pure ()
+--        dbg_trace f!"Success for (m, n) = ({m}, {n})."
+      else
+        IO.println f!"Failure for (m, n) = ({m}, {n})."
 
 end TPwL
