b90c67bd27f20085d2ff53121c5607d9c7f786de
Damiano Testa <d.testa@warwick.ac.uk>
2023-11-07 10:53:58 +0000

    Make IsSquare more explicit

--- MA4N1_2023/IsSquare.lean
+++ MA4N1_2023/IsSquare.lean
@@ -1,13 +1,12 @@
 import Mathlib.Tactic
 
+theorem IsSquare_iff_mul_self {m : ℕ} : IsSquare m ↔ Nat.sqrt m * Nat.sqrt m = m := by
+  rw [← Nat.exists_mul_self m, IsSquare]
+  simp_rw [eq_comm]
+  done
+
 instance : DecidablePred (@IsSquare ℕ _) :=
-  fun m => decidable_of_iff' (Nat.sqrt m * Nat.sqrt m = m) <| by
-    simp_rw [←Nat.exists_mul_self m, IsSquare, eq_comm]
+  fun m => decidable_of_iff' (Nat.sqrt m * Nat.sqrt m = m) IsSquare_iff_mul_self
 
-def no_three_square_product (s : Finset ℕ) : Prop :=
-  ∀ x y z : s, x ≠ y ∧ y ≠ z ∧ z ≠ x → ¬IsSquare (x * y * z : ℕ)
-
-instance : DecidablePred no_three_square_product := fun _ => Fintype.decidableForallFintype
-
-theorem main : no_three_square_product {2, 3, 4, 5} := by
+example : IsSquare 36 := by
   decide
