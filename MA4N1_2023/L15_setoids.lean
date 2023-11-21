96d9cc9292a32894b800a0ef8d311a7a20420d4e
adomani <adomani@gmail.com>
2023-11-21 00:56:14 +0000

    Add Week to Quotient

--- MA4N1_2023/L15_setoids.lean
+++ MA4N1_2023/L15_setoids.lean
@@ -1,4 +1,5 @@
 import Mathlib.Tactic
+import MA4N1_2023.help_me
 
 namespace TPwL
 
@@ -111,7 +112,113 @@ example : (⟦a⟧ : Quotient Nat_setoid) = ⟦b⟧ ↔ a ≈ b := by
   done
 
 #check Quotient.eq_rel
-
+#check Quot.sound
 end
+
+/-- `Week` is the finite Type with exactly 7 constructors, one for each day of the week. -/
+inductive Week
+  | Mon
+  | Tue
+  | Wed
+  | Thu
+  | Fri
+  | Sat
+  | Sun
+
+/-- `week_end? d` is the predicate on a day of the `Week`,
+answering the question "Is `d` part of the week-end?" -/
+def week_end? : Week → Bool
+  -- the "anonymous dot-notation":
+  -- Lean is expecting a term of type `Week`, so `.Sat` gets parsed as `Week.Sat
+  | .Sat => true
+  | .Sun => true
+  -- every other day of the `Week` returns `false`
+  | _ => false
+
+instance Week_setoid : Setoid Week where
+  r x y := week_end? x = week_end? y
+  iseqv := by
+    constructor
+    · exact? says exact fun x => rfl
+    · exact? says exact fun {x y} a => id a.symm
+    · intros x y z xy yz
+      exact xy.trans yz
+    done
+
+--  If you like an equivalent but more obfuscated version of the instance above, here it is!
+/-
+instance : Setoid Week where
+  r := (work? · = work? ·)
+  iseqv := { refl  := fun _ => rfl
+             symm  := fun {_ _} => .symm
+             trans := fun {_ _ _} => .trans }
+-/
+
+@[simp]
+lemma sat_sun : (⟦Week.Sat⟧ : Quotient Week_setoid) = ⟦Week.Sun⟧ := by
+  exact Quotient.eq.mpr rfl
+  done
+
+example : (⟦Week.Sat⟧ : Quotient Week_setoid) ≠ ⟦Week.Mon⟧ := by
+  intro h
+  simp only [Quotient.eq] at h
+  cases h
+  done
+
+example : (⟦Week.Sat⟧ : Quotient Week_setoid) ≠ ⟦Week.Mon⟧ := by
+  simp only [ne_eq, Quotient.eq]
+  rintro ⟨⟩
+  done
+
+@[simp]
+lemma equiv_class_of_Sunday (d : Week) : (⟦Week.Sun⟧ : Quotient Week_setoid) = ⟦d⟧ ↔
+    d ∈ ({Week.Sat, Week.Sun} : Set _) := by
+  -- `rcases d with _ | _ | _ | _ | _ | _ | _ <;>` also works instead of `induction`
+  induction d <;>
+    simp <;>
+    rintro ⟨⟩
+  done
+
+@[simp]
+lemma equiv_class_of_Monday (d : Week) : (⟦Week.Mon⟧ : Quotient Week_setoid) = ⟦d⟧ ↔
+    d ∈ ({Week.Mon, Week.Tue, Week.Wed, Week.Thu, Week.Fri} : Set _) := by
+  -- `rcases d with _ | _ | _ | _ | _ | _ | _ <;>` also works instead of `induction`
+  induction d <;>
+    simp <;> try rfl
+  · rintro ⟨⟩
+  · rintro ⟨⟩
+  done
+
+--  `≃` is the symbol for an `Equiv
+#print Equiv
+
+/-!
+An `Equiv` is a structure with two input types `α` and `β` and 4 fields:
+* `toFun`     -- a function `α → β`;
+* `invFun`    -- a function `β → α`;
+* `left_inv`  -- a proof that `invFun` is a left-inverse to `toFun`;
+* `right_inv` -- a proof that `invFun` is a right-inverse to `toFun`.
+-/
+
+def Equiv.ff : Quotient Week_setoid ≃ Bool where
+  -- the function from the quotient to `Bool` is the `Quotient.lift` of the function `week_end?`.
+  -- in maths, it is more common to say that `week_end?` "descends" to the quotient.
+  toFun := Quotient.lift week_end? (fun a b => id)
+  -- the function from `Bool` to the quotient assigns `true` to `⟦.Sun⟧` and `false` to `⟦.Mon⟧`
+  invFun b := if b then ⟦.Sun⟧ else ⟦.Mon⟧
+  -- it is now up to you to prove that these two functions are inverses of each other!
+  -- the tactic `hint_inverse` will give you a hint on how to start the proof!
+  left_inv := by
+    rintro ⟨d⟩
+    dsimp only
+    split_ifs
+    · apply (equiv_class_of_Sunday d).mpr
+      induction d <;> aesop
+    · apply (equiv_class_of_Monday d).mpr
+      induction d <;> aesop
+    done
+  right_inv := by
+    simp only
+    done
 
 end TPwL
