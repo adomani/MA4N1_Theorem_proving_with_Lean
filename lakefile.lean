c0f6d66ec1a1127bfccd143cb25d65be7ccddce5
adomani <adomani@gmail.com>
2024-09-20 17:26:28 +0200

    update options

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,10 @@ open Lake DSL
 
 package «mA4N1_2023» where
   -- add any package configuration options here
-  moreServerArgs := #[ "-DautoImplicit=false" ]
+  moreServerOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩
+  ]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
